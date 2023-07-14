020d9458cd6a580e25e19ee2fe66b0034c6886af
kikio2022 <kikio_2127@hotmail.com>
2023-07-14 02:40:24 -0300

    modificado

--- Proyecto-Final/src/app/app.module.ts
+++ Proyecto-Final/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    AboutComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

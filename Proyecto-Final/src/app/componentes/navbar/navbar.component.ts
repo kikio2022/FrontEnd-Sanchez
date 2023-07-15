5b3ecde71d68b7db743c20a981a4cc29ac252149
kikio2022 <kikio_2127@hotmail.com>
2023-07-15 01:46:25 -0300

    Pasando componentes/creación del servicio.

--- Proyecto-Final/src/app/componentes/navbar/navbar.component.ts
+++ Proyecto-Final/src/app/componentes/navbar/navbar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+miportfolio:any;
+  constructor(private datosPortfolio:PortfolioService){}
 
-}
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miportfolio=data;
+    });
+  }
+  }
+  
+
+
+

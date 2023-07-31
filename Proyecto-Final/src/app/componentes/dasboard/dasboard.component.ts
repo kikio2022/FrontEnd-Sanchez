6dd988abeea7cdb6ccfa7eb498894163b6cff186
kikio2022 <kikio_2127@hotmail.com>
2023-07-31 01:38:20 -0300

    pasando modales a los componentes problemas con el bucle, de los botones no aparecia el modal(solucionado)

--- Proyecto-Final/src/app/componentes/dasboard/dasboard.component.ts
+++ Proyecto-Final/src/app/componentes/dasboard/dasboard.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-dasboard',
   templateUrl: './dasboard.component.html',
   styleUrls: ['./dasboard.component.css']
 })
-export class DasboardComponent {
+export class DasboardComponent implements OnInit{
+botones:any;
 
+constructor(private datosPortfolio:PortfolioService){}
+
+ngOnInit(): void {
+  this.datosPortfolio.obtenerDatos().subscribe(data =>{
+    console.log(data);
+    this.botones=data.dashboard;
+  })
+}
 }
